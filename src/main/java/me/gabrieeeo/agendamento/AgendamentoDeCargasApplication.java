0ff8dfbe9f1ebafbb86140d583df536cfd437e0c
gabriellima <gabrieeelima15@gmail.com>
2025-05-28 18:06:08 -0300

    inicial: adicionando projeto

---
+++ src/main/java/me/gabrieeeo/agendamento/AgendamentoDeCargasApplication.java
@@ -0,0 +1,13 @@
+package me.gabrieeeo.agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoDeCargasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoDeCargasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/gabrieeeo/agendamento/AgendamentoDeCargasApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabrieeeo.agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoDeCargasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
