51251c667e06c1a758bb334173092ad18a670e60
gabriellima <gabrieeelima15@gmail.com>
2025-05-29 17:12:22 -0300

    feat: adicionando FornecedorService.java

---
+++ src/main/java/me/gabrieeeo/agendamento/service/AgendamentoService.java
@@ -0,0 +1,31 @@
+package me.gabrieeeo.agendamento.service;
+
+import me.gabrieeeo.agendamento.model.Agendamento;
+import me.gabrieeeo.agendamento.repository.AgendamentoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AgendamentoService {
+
+    private final AgendamentoRepository agendamentoRepository;
+
+    public AgendamentoService(AgendamentoRepository agendamentoRepository) {
+        this.agendamentoRepository = agendamentoRepository;
+    }
+
+    public Agendamento salvar(Agendamento agendamento) {
+        return agendamentoRepository.save(agendamento);
+    }
+
+    public List<Agendamento> listarTodos() {
+        return agendamentoRepository.findAll();
+    }
+
+    public Optional<Agendamento> buscarPorId(Long id) {
+        return agendamentoRepository.findById(id);
+    }
+
+}
